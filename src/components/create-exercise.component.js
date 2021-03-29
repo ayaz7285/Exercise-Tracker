32faec079fd785b3ffca98393d8e45f63ed69418
ayaz7285 <ayaz.3.sarwarchem@gmail.com>
2021-03-29 16:12:21 +0530

    fourth

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/users/')
+    axios.get('http://localhost:5000/users/'||'https://zaya-app.herokuapp.com/users/')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
@@ -74,7 +74,7 @@ export default class CreateExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercises/add', exercise)
+    axios.post('http://localhost:5000/exercises/add'||'https://zaya-app.herokuapp.com/exercises/add', exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
